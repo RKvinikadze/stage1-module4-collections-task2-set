ce742aeaa38c1c18624d7df374cf9247de6d5296
romikokvinikadze <r.kvinikadze11@gmail.com>
2022-08-19 07:55:16 +0400

    sets

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,22 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> res = new HashSet<>();
+
+        for (int x: sourceList){
+            if (x % 2 != 0){
+                res.add(x);
+                res.add(x * 2);
+            }else{
+                int k = x;
+                while (k % 2 != 1){
+                    res.add(k);
+                    k /= 2;
+                }
+                res.add(k);
+            }
+        }
+
+        return res;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -1,9 +1,13 @@
 package com.epam.mjc.collections.set;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        NavigableSet<Integer> res = new TreeSet<>();
+        for (int x: sourceList)
+            res.add(x * x);
+
+        return res.subSet(lowerBound, upperBound + 1);
     }
 }
